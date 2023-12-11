e12e0b4accd642a945764c63a75fcacea9dd1aab
Jacob Mischka <jacob@mischka.me>
2023-12-11 02:51:39 -0600

    Finish day 10
    Flood too hard

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -7,7 +7,8 @@ use adventofcode_2023::grid::*;
 
 fn main() {
     let mut start: Option<Position> = None;
-    let grid = Grid(
+    let mut start_vectors: Vec<Vector> = Vec::new();
+    let mut grid = Grid(
         io::stdin()
             .lines()
             .enumerate()
@@ -44,12 +45,12 @@ fn main() {
             for dx in (-1)..=1 {
                 if let Ok(potential_pos) = start + Vector(dx, dy) {
                     if let Ok(vectors) = tile_ends(grid.get_pos(potential_pos).unwrap()) {
-                        if vectors
-                            .iter()
-                            .any(|vector| potential_pos + *vector == Ok(start))
-                        {
-                            pos = Some(potential_pos);
-                            break 'outer;
+                        for vector in vectors {
+                            if potential_pos + vector == Ok(start) {
+                                pos = Some(potential_pos);
+                                start_vectors.push(Vector(vector.0 * -1, vector.1 * -1));
+                                break 'outer;
+                            }
                         }
                     }
                 }
@@ -75,110 +76,38 @@ fn main() {
 
         pipe_loop.insert(pos, step);
     }
+    start_vectors.push(prev - pos);
 
     println!("Part 1: {}", step / 2 + step % 2);
 
-    let mut outside_visited: HashSet<Position> = HashSet::new();
-    for y in 0..grid.len() {
-        for x in 0..grid[y].len() {
-            if y != 0 && y != grid.len() - 1 && x != 0 && x != grid[y].len() - 1 {
-                continue;
-            }
-
-            let pos = Position(x, y);
-            if !pipe_loop.contains_key(&pos) {
-                outside_visited.insert(pos);
-                flood(pos, &grid, &pipe_loop, &mut outside_visited);
-            }
+    for possible_tile in ['|', '-', 'L', 'J', '7', 'F'] {
+        let vectors = tile_ends(possible_tile).unwrap();
+        if vectors.into_iter().all(|v| start_vectors.contains(&v)) {
+            grid[start.1][start.0] = possible_tile;
+            break;
         }
     }
 
-    let mut part2 = 0;
+    let mut inside_positions: HashSet<Position> = HashSet::new();
     for y in 0..grid.len() {
+        let mut inside = false;
         for x in 0..grid[y].len() {
             let pos = Position(x, y);
-            if !pipe_loop.contains_key(&pos) && !outside_visited.contains(&pos) {
-                println!("contained: {pos:?}");
-                part2 += 1;
-            }
-        }
-    }
-
-    println!("Part 2: {part2}");
-}
-
-// fn can_visit(
-//     from: Position,
-//     vector: Vector,
-//     grid: &Grid,
-//     pipe_loop: &HashMap<Position, usize>,
-// ) -> bool {
-//     match from + vector {
-//         Some(to) => {
-//
-//         },
-//         None => false,
-//     }
-// }
-
-fn flood(
-    pos: Position,
-    grid: &Grid,
-    pipe_loop: &HashMap<Position, usize>,
-    visited: &mut HashSet<Position>,
-) {
-    for y in -1..=1 {
-        for x in -1..=1 {
-            let vector = Vector(x as isize, y as isize);
-            if vector.manhattan_distance() > 1 {
-                continue;
-            }
-
-            if let Ok(potential_pos) = pos + vector {
-                if !visited.contains(&potential_pos) {
-                    if let Some(potential_tile) = grid.get_pos(potential_pos) {
-                        if !pipe_loop.contains_key(&potential_pos) || {
-                            let potential_step = *pipe_loop.get(&potential_pos).unwrap();
-
-                            if vector.0 == 0 {
-                                (match potential_pos + Vector(-1, 0) {
-                                    Ok(adj) => match pipe_loop.get(&adj) {
-                                        Some(adj_step) => adj_step.abs_diff(potential_step) != 1,
-                                        _ => false,
-                                    },
-                                    _ => false,
-                                }) || (match potential_pos + Vector(1, 0) {
-                                    Ok(adj) => match pipe_loop.get(&adj) {
-                                        Some(adj_step) => adj_step.abs_diff(potential_step) != 1,
-                                        _ => false,
-                                    },
-                                    _ => false,
-                                })
-                            } else {
-                                (match potential_pos + Vector(0, -1) {
-                                    Ok(adj) => match pipe_loop.get(&adj) {
-                                        Some(adj_step) => adj_step.abs_diff(potential_step) != 1,
-                                        _ => false,
-                                    },
-                                    _ => false,
-                                }) || (match potential_pos + Vector(0, 1) {
-                                    Ok(adj) => match pipe_loop.get(&adj) {
-                                        Some(adj_step) => adj_step.abs_diff(potential_step) != 1,
-                                        _ => false,
-                                    },
-                                    _ => false,
-                                })
-                            }
-                        } {
-                            visited.insert(potential_pos);
-                            println!("visited {potential_pos:?}");
-                            flood(potential_pos, grid, pipe_loop, visited);
-                        }
-                    }
+            if pipe_loop.contains_key(&pos) {
+                let tile = grid.get_pos(pos).unwrap();
+                let vs = tile_ends(tile).unwrap();
+                if vs.into_iter().any(|v| v.1 < 0) {
+                    inside = !inside;
+                }
+            } else {
+                if inside {
+                    inside_positions.insert(pos);
                 }
             }
         }
     }
+
+    println!("Part 2: {}", inside_positions.len());
 }
 
 fn tile_ends(tile: char) -> Result<[Vector; 2], String> {
